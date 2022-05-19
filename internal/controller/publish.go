52b50186d7aa4d3ed9d5d864e1a910ad7dce91cd
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2022-05-19 13:08:59 +0800

    fix: 规范返回参数

--- internal/controller/publish.go
+++ internal/controller/publish.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yunyandz/tiktok-demo-backend/internal/errorx"
 	"github.com/yunyandz/tiktok-demo-backend/internal/jwtx"
 	"github.com/yunyandz/tiktok-demo-backend/internal/service"
 )
@@ -14,33 +15,35 @@ func (ctl *Controller) Publish(c *gin.Context) {
 	uc, err := jwtx.ParseUserClaims(token)
 	if err != nil {
 		ctl.logger.Sugar().Errorf("ParseUserClaims error: %v", err)
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"status": -1,
-			"msg":    err.Error(),
+		c.JSON(http.StatusUnauthorized, service.Response{
+			StatusCode: -1,
+			StatusMsg:  err.Error(),
 		})
 		return
 	}
 	data, err := c.FormFile("data")
 	if err != nil {
 		ctl.logger.Sugar().Errorf("FormFile error: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": -1,
-			"msg":    err.Error(),
+		c.JSON(http.StatusBadRequest, service.Response{
+			StatusCode: -1,
+			StatusMsg:  errorx.ErrReadVideo.Error(),
 		})
 		return
 	}
 	file, err := data.Open()
 	if err != nil {
 		ctl.logger.Sugar().Errorf("data.Open error: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status": -1,
-			"msg":    err.Error(),
+		c.JSON(http.StatusBadRequest, service.Response{
+			StatusCode: -1,
+			StatusMsg:  errorx.ErrReadVideo.Error(),
 		})
 		return
 	}
-	ctl.service.PublishVideo(context.Background(), uc.UserID, data.Filename, file)
+	// TODO: 判断文件类型是否为视频文件
+	res := ctl.service.PublishVideo(context.Background(), uc.UserID, data.Filename, file)
 	c.JSON(http.StatusOK, service.Response{
-		StatusCode: 0,
+		StatusCode: res.StatusCode,
+		StatusMsg:  res.StatusMsg,
 	})
 }
 
