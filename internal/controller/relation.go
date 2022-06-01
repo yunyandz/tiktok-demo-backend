54a163f5f75a9483b756eb05e8c820c08ce0fcd4
chenzhenyu-1 <chen_zhenyu_1@163.com>
2022-06-01 18:24:16 +0800

    fix: 修正错别字

--- internal/controller/relation.go
+++ internal/controller/relation.go
@@ -28,7 +28,7 @@ func (ctl *Controller) RelationAction(c *gin.Context) {
 		return
 	}
 	if req.ToUserId == req.UserId {
-		c.JSON(http.StatusOK, service.Response{StatusCode: 1, StatusMsg: "不能对自己继续操作"})
+		c.JSON(http.StatusOK, service.Response{StatusCode: 1, StatusMsg: "不能对自己进行操作"})
 		return
 	}
 	const Follow = 1
